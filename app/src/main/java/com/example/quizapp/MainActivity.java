91c19b3d0012c5a9dbf59a15cc11d2a5492ddc57
HananAlaskar <hanan.f.alaskar@gmail.com>
2020-04-02 12:11:21 +0300

    get answer of question one

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,12 +3,56 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addClickListener();
+    }
+
+    private void addClickListener() {
+
+        findViewById(R.id.submit_btn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()) {
+            case R.id.submit_btn:
+                submitQuiz();
+                break;
+
+        }
+
+    }
+
+    private void submitQuiz() {
+
+        getAnswer();
+
+    }
+
+    private void getAnswer() {
+
+        String questionOneAnswer = getQuestionOneAnswer();
+    }
+
+    private String getQuestionOneAnswer() {
+
+        RadioGroup questionOneRadioGroup = findViewById(R.id.question_one_answer);
+        int selectedRadioButtonid = questionOneRadioGroup.getCheckedRadioButtonId();
+        RadioButton selectedRadioButton = findViewById(selectedRadioButtonid);
+        String questionOneAnswer = selectedRadioButton.getText().toString();
+
+        return questionOneAnswer;
     }
 }
